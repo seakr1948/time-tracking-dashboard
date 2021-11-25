f0cd65d8aea692f881556475851f8f4f7c51e92c
seakr1948 <seakr1948@gmail.com>
2021-11-24 20:48:13 -0800

    all a tags switched to buttons to fix deployment issues

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -22,9 +22,9 @@ const ActivityCard = ({data, timeframe}) => {
             <div className="activity-content flex">
                 <div className="activity-header flex">
                     <p>{data.title}</p>
-                    <a target="_self">
+                    <button>
                         <svg className="ellipse" width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
-                    </a>
+                    </button>
                     
                 </div>
                 <div className="activity-tracker flex">
